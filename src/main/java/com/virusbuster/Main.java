38d30fac24a367c5cdba1b4234f8d6d10fd11061
aeroninou <aeroninouye@live.com>
2023-01-26 10:13:14 -1000

    added game class and commands enum values AB#152

--- src/main/java/com/virusbuster/Main.java
+++ src/main/java/com/virusbuster/Main.java
@@ -1,6 +1,7 @@
 package com.virusbuster;
 
 import com.virusbuster.controller.Controller;
+import com.virusbuster.model.Game;
 import com.virusbuster.view.View;
 
 public class Main {
@@ -10,17 +11,20 @@ public class Main {
 
         //Display Game Title
         //Game Summary
-        controller.startGame();
+        //controller.startGame();
 
-        String userInput = controller.userInput();
+//        String userInput = controller.userInput();
+//
+//        //user input and play game or quit game.
+//        //also to play the game again.
+//        if("P".equals(userInput)){
+//            controller.startGame();
+//        } else{
+//            controller.loserMessage();
+//        }
 
-        //user input and play game or quit game.
-        //also to play the game again.
-        if("P".equals(userInput)){
-            controller.startGame();
-        } else{
-            controller.loserMessage();
-        }
+
+        Game.gameTest();
 
     }
 
---
+++ src/main/java/com/virusbuster/model/Game.java
@@ -0,0 +1,52 @@
+package com.virusbuster.model;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Game {
+
+    private static Commands move;
+
+
+    public static void gameTest (){
+        boolean inputVaild = false;
+        while (!inputVaild){
+            System.out.println('>');
+            String [] moveInput = commandInput().toLowerCase().split(" ");
+
+            if ("look".equals(moveInput[0])){
+                move = validMove(moveInput[0]);
+                System.out.println("You chose " + move);
+                inputVaild = true;
+            } else if ("bag".equals(moveInput[0])){
+                move = validMove(moveInput[0]);
+                System.out.println("You chose " + move);
+                inputVaild = true;
+            }
+        }
+    }
+
+
+    private static Commands validMove(String input) {
+        Commands result = null;
+        for (Commands command : values()) {
+            if (command.getValue().equalsIgnoreCase(input)) {
+                result = command;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private static Commands[] values() {
+        return Commands.values();
+    }
+
+    private static String commandInput() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine().trim();
+        return input;
+    }
+
+}
