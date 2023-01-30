83ba187cf6b556ff07ecdbc9520de317ee19a802
Veronica Pichay <veronicapichay@gmail.com>
2023-01-30 12:09:58 -0800

    AB#155

--- src/main/java/com/virusbuster/model/Item.java
+++ src/main/java/com/virusbuster/model/Item.java
@@ -1,4 +1,25 @@
 package com.virusbuster.model;
 
 public class Item {
+
+    private String name;
+
+    //ctor
+    public Item (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public static void displayItemDescription() {
+
+    }
+
 }
